17bcafc6fa55534c7052df3cf200c63f427fb7ae
sadsock <mantis-shrimp@outlook.com>
2023-10-21 21:28:38 +0800

    switch packer to lazy

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ require("mason").setup({
 })
 
 require("mason-lspconfig").setup {
-    ensure_installed = { "sumneko_lua", "rust_analyzer", "fortls", "marksman", "jedi_language_server"},
+    ensure_installed = { "lua_ls", "rust_analyzer", "fortls", "marksman", "jedi_language_server"},
 }
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
